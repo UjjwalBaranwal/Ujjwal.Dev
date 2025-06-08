50e58314227e86104343b6d70f5a18f7612740e6
ujjwal <ujjwalbaranwal221715@gmail.com>
2025-06-08 17:49:52 +0530

    added the vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,28 @@ import Certifications from "./_components/Certifications";
 import Contact from "./_components/Contact";
 import Footer from "./_components/Footer";
 import ScrollToTop from "./_components/ScrollToTop";
-
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
-    <div className="flex flex-col min-h-screen bg-background-primary text-white">
-      <Navbar />
+    <>
+      <div className="flex flex-col min-h-screen bg-background-primary text-white">
+        <Navbar />
 
-      <main className="flex-grow">
-        <Hero />
-        <About />
-        <Skills />
-        <Education />
-        <Certifications />
-        <Projects />
-        <Contact />
-      </main>
+        <main className="flex-grow">
+          <Hero />
+          <About />
+          <Skills />
+          <Education />
+          <Certifications />
+          <Projects />
+          <Contact />
+        </main>
 
-      <Footer />
-      <ScrollToTop />
-    </div>
+        <Footer />
+        <ScrollToTop />
+      </div>
+      <Analytics />
+    </>
   );
 }
 
